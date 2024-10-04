4c3b0513b446d248ce75cf791aeff157c1080035
anhhiu <dtc2054801360068@ictu.edu.vn>
2024-10-04 09:20:41 +0700

    4/10 cấu hình thêm tệp .env, sử dụng thư viện godotenv để đọc tep .env

--- config/db.go
+++ config/db.go
@@ -3,7 +3,8 @@ package config
 import (
 	"fmt"
 	"log"
-
+	"os"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
 )
@@ -11,12 +12,42 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	//dsn := "sqlserver://sa:hao123@FF01:1433?database=demobibi&instance=HAHAO"
+	/* //dsn := "sqlserver://sa:hao123@FF01:1433?database=demobibi&instance=HAHAO"
 	dsn := "sqlserver://sa:hao123@LAPTOP-7CAHEI3Q:1433?database=demobibi&instance=HATHANHHAO"
 	database, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("ket noi khong thanh cong", err)
 	}
 	fmt.Println("con nect thanh cong")
-	DB = database
+	DB = database */
+	// Tải tệp .env
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Không thể tải tệp .env")
+    }
+
+    // Lấy thông tin từ biến môi trường
+    dbUser := os.Getenv("DB_USER")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dbHost := os.Getenv("DB_HOST")
+    dbPort := os.Getenv("DB_PORT")
+    dbName := os.Getenv("DB_NAME")
+    dbInstance := os.Getenv("DB_INSTANCE")
+
+    // Kiểm tra các giá trị để đảm bảo rằng chúng không bị trống
+    if dbUser == "" || dbPassword == "" || dbHost == "" || dbPort == "" || dbName == "" || dbInstance == "" {
+        log.Fatal("Thiếu thông tin kết nối cơ sở dữ liệu")
+    }
+
+    // Tạo chuỗi kết nối với SQL Server
+    dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s&instance=%s",
+        dbUser, dbPassword, dbHost, dbPort, dbName, dbInstance)
+
+    // Mở kết nối đến cơ sở dữ liệu
+    database, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Kết nối không thành công:", err)
+    }
+    fmt.Println("Kết nối thành công")
+    DB = database
 }
